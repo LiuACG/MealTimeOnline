171ace2d7a4d3bdc713eb2b7aa8433db10d33258
AK4TRL <ak4trl@gmail.com>
2016-12-05 20:58:41 +0800

    +add Order page

---
+++ MealTimeOnline/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MealTimeOnline.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order/Canteen
+        [HttpGet]
+        public ActionResult Canteen()
+        {
+            return View();
+        }
+    }
+}
