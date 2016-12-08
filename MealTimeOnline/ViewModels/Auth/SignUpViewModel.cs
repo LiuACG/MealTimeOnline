0cc9eaa448eff24f00f6de42439a2726294af57c
AK4TRL <ak4trl@gmail.com>
2016-12-08 08:41:02 +0800

    +add fontawesome & modify Login, Signup page

--- MealTimeOnline/ViewModels/Auth/SignUpViewModel.cs
+++ MealTimeOnline/ViewModels/Auth/SignUpViewModel.cs
@@ -17,11 +17,18 @@ namespace MealTimeOnline.ViewModels.Auth
         public string Username { get; set; }
 
         [DisplayName("Password")]
-        [Required(ErrorMessage = "密码不能为空")]
+        [Required(ErrorMessage = "新密码不能为空")]
         [MinLength(6, ErrorMessage = "密码长度不能小于6位")]
         [MaxLength(32, ErrorMessage = "密码长度不能大于32位")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DisplayName("Current Password")]
+        [Required(ErrorMessage = "现有密码不能为空")]
+        [MinLength(6, ErrorMessage = "密码长度不能小于6位")]
+        [MaxLength(32, ErrorMessage = "密码长度不能大于32位")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
 
         [DisplayName("Confirm Password")]
         [Required(ErrorMessage = "请再次输入密码")]
