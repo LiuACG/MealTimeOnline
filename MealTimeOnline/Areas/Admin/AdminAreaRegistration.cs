43d498afd0a4f7b6067c8aa6b264cdf36c70c6ac
LiuACG <LiuACG@163.com>
2016-12-15 01:48:43 +0800

    + Food and People Category

--- MealTimeOnline/Areas/Admin/AdminAreaRegistration.cs
+++ MealTimeOnline/Areas/Admin/AdminAreaRegistration.cs
@@ -9,6 +9,12 @@ namespace MealTimeOnline.Areas.Admin
         public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
+                name: "Admin Images",
+                url: "Image/{action}/{width}x{height}/{text}",
+                defaults: new { controller = "Image", action = "Placeholder", text = UrlParameter.Optional }
+            );
+
+            context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
